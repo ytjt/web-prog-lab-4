4024878310a594b9bbb6973da4c04b27aca81849
ytjt <102714570+ytjt@users.noreply.github.com>
2022-06-12 12:17:11 +0800

    question 1 file

---
+++ q1.js
@@ -0,0 +1,5 @@
+let a,b,c;
+
+a = document.getElementById("text").style.color = 'red';
+b = document.getElementById("background").style.backgroundColor = 'yellow';
+c= document.getElementById("text").style.fontSize = '40px';
